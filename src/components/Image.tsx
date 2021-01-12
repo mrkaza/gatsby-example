cfc0576e579f1ceb2f93007ae555939bc9aeb4d6
mrkaza <marko.mrkonjic97@gmail.com>
2021-01-12 11:13:57 +0100

    image remade

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,28 +4,23 @@ import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 export const Image = () => {
-  // const path = 'gatsby-astronaut.png';
-
   const data = useStaticQuery(graphql`
-    query Images {
-      file(relativePath: { eq: "gatsby-astronaut.png" }) {
+    query {
+      images: file(relativePath: { eq: "gatsby-astronaut.png" }) {
         childImageSharp {
-          fluid {
-            srcWebp
-            ...GatsbyImageSharpFluid
+          fluid(maxWidth: 568) {
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
     }
   `);
 
-  console.log(data.file.childImageSharp.fluid);
+  console.log(data);
 
-  if (!data?.file?.childImageSharp?.fluid) {
+  if (!data?.images?.childImageSharp?.fluid) {
     return <div>Picture not found</div>;
   }
 
-  return <Img fluid={data.file.childImageSharp.fluid} />;
-
-  // return <div>heeej</div>;
+  return <Img {...data.images.childImageSharp} />;
 };
